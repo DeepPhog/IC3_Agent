4e7edd1bbf2ddc94747272e81797ef5daf3f9f28
DeepPhog <mrsan@online.no>
2022-11-24 12:22:22 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+//var
+var btn = document.getElementById("btn");
+var h = document.getElementById("h");
+var m = document.getElementById("m");
+
+var user = [
+  {
+    email: "Em",
+    passord: "test",
+  },
+  {
+    email: "ail",
+    passord: "pass",
+  },
+  {
+    email: "Email",
+    passord: "testpass",
+  },
+];
+var len = user.length;
+//login
+
+//if
+btn.onclick = function myfunc() {
+  var test = 0;
+  var mail = document.getElementById("Email").value;
+  var pass = document.getElementById("pass").value;
+
+  h.removeAttribute("class");
+  h.setAttribute("class", "hidden");
+  m.removeAttribute("class");
+  m.setAttribute("class", "hidden");
+  for (var i = 0; i < len; i++) {
+    if (mail == user[i].email) {
+      if (pass == user[i].passord) {
+        console.log("Right pass");
+        window.location.href = "index.html";
+      } else {
+        console.log("wrong pass");
+        h.removeAttribute("class");
+        h.setAttribute("class", "red");
+      }
+    } else {
+      test++;
+    }
+    if (test == 3) {
+      console.log("wrong email");
+      m.removeAttribute("class");
+      m.setAttribute("class", "red");
+    }
+  }
+};
